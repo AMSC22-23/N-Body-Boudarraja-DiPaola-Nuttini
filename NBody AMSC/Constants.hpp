18153326f310801c071604a280ef8c07e2b55ba4
GDipa2000 <149886145+GDipa2000@users.noreply.github.com>
2023-12-18 15:03:22 +0100

    Update Constants.hpp
    updated the header file constants.hpp where we have defined some global variables

--- NBody AMSC/Constants.hpp
+++ NBody AMSC/Constants.hpp
@@ -3,18 +3,31 @@ ATTENZIONE!!!
 Il seguente header serve a definire le costanti del problema e deve essere ancora implementato in una maniera coerente 
 per poter essere utilizzato intelligentemente nel programma
 
-(Generato da CHAT-GPT!!!)
-*/
-
-#ifndef CONSTANTS_H
+(#ifndef CONSTANTS_H
 #define CONSTANTS_H
+
 
 //Dimensione del problema 1D 2D 3D
 const unsigned int dim=3;
 
+
 //Costante Gravitazionale Universale G
 
-//Delta temporale
+
+
+
+//Delta temporale espresso in secondi
+const double dt=1;
+      
+//Tempo totale della simulazione espresso in secondi
+const double totalTime =5;
+
+
+//Numeri di cicli
+const int cycles = static_cast<int>(std::floor(totalTime/dt));
+
+//Numero di particelle generate                 //modifica del 17 dicembre 2023, è stata creata una costante per definire 
+const int numberOfParticles = 100;              //il numero di particelle da inizializzare per la simulazione.
 
 
 #endif  // CONSTANTS_H
