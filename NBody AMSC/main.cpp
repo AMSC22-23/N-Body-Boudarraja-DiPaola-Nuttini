2cabbb27d40840500510b0e3d2373f7835b2f1aa
GDipa2000 <149886145+GDipa2000@users.noreply.github.com>
2023-12-18 21:32:51 +0100

    Update main.cpp
    The main has been update to manage the serial and the parallel version of the n-body problem, so now we have an if-cycle meant to select the type of execution and it is also included the simulationFunctionParallel.hpp where we have implemented the openMp parallelization

--- NBody AMSC/main.cpp
+++ NBody AMSC/main.cpp
@@ -2,12 +2,26 @@
 #include "Constants.hpp"
 #include "Particle.hpp"
 #include "simulationfunctions.hpp"
+#include "simulationFunctionParallel.hpp"
 
+/*
+il main viene principalmente utilizzato per avviare il programma, al suo interno sono contenuti gli header files delle classi utilizzate per la simulazione
+tra cui 
+1)Costants.hpp -> header file utilizzato per definire delle costanti che verranno utilizzate nel programma
+2)Particle.hpp ->header file in cui è contenuta la classe che rappresenta il corpo di una particella
+3)simulationfunctions.hpp -> La classe in cui viene eseguita effettivamente la simulazione
+il seguente blocco di programma di occupa di chiamare il metodo statico doSim() che  da il via alla simulazione
+*/
 
 int main(){
-    double dt=100.0;
     
-    simulationfunctions::executeSim();
+    bool parallel=true;
+    
+    if (parallel==true){
+        simulationFunctionsParallel::doParallelSim();
+    } else {
+        simulationfunctions::doSim();
+    }
+    
     return 0;
 }
-
