4ec79e6bf03000f650519c47d1c67df55a753baa
GDipa2000 <149886145+GDipa2000@users.noreply.github.com>
2023-12-21 17:02:50 +0100

    Update Constants.hpp
    updated the gravitational constant

--- NBody AMSC/Constants.hpp
+++ NBody AMSC/Constants.hpp
@@ -1,11 +1,13 @@
 /*
 ATTENZIONE!!!
-Il seguente header serve a definire le costanti del problema e deve essere ancora implementato in una maniera coerente 
+Il seguente header serve a definire le costanti del problema e deve essere ancora implementato in una maniera coerente
 per poter essere utilizzato intelligentemente nel programma
 
-(#ifndef CONSTANTS_H
-#define CONSTANTS_H
+*/
 
+
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
 
 //Dimensione del problema 1D 2D 3D
 const unsigned int dim=3;
@@ -13,21 +15,22 @@ const unsigned int dim=3;
 
 //Costante Gravitazionale Universale G
 
+const double G = -6.67408e-11;
 
 
 
 //Delta temporale espresso in secondi
-const double dt=1;
+double dt=0.1;
       
 //Tempo totale della simulazione espresso in secondi
-const double totalTime =5;
+ double totalTime =1000;
 
 
 //Numeri di cicli
 const int cycles = static_cast<int>(std::floor(totalTime/dt));
 
 //Numero di particelle generate                 //modifica del 17 dicembre 2023, è stata creata una costante per definire 
-const int numberOfParticles = 100;              //il numero di particelle da inizializzare per la simulazione.
+const int numberOfParticles = 50;              //il numero di particelle da inizializzare per la simulazione.
 
 
 #endif  // CONSTANTS_H
